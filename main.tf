ab4e573cab841aa1265fec4a2902476d882a522a
Aleksander Fidelus <aleksander.fidelus@hitachivantara.com>
2023-11-10 18:48:53 +0100

    AWS Fastapi stack

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    spacelift = {
+      source = "spacelift-io/spacelift"
+      version = "1.6.0"
+    }
+  }
+}
+
+provider "spacelift" {
+}
+
+resource "spacelift_stack" "aws_fastapi" {
+    name        = "AWS Fastapi"
+    description = "Provisions a FastAPI application on AWS"
+    repository  = "aws-fastapi"
+    project_root = "infra"
+    branch      = "master"
+    autodeploy  = true
+    import_state_file = "states/state.tfstate"
+}
