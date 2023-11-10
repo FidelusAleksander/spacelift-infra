d46e34104e481bff880f205a87d034e2a3ee404e
Aleksander Fidelus <aleksander.fidelus@hitachivantara.com>
2023-11-10 18:58:39 +0100

    Remove import state

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     spacelift = {
-      source = "spacelift-io/spacelift"
+      source  = "spacelift-io/spacelift"
       version = "1.6.0"
     }
   }
@@ -11,11 +11,10 @@ provider "spacelift" {
 }
 
 resource "spacelift_stack" "aws_fastapi" {
-    name        = "AWS Fastapi"
-    description = "Provisions a FastAPI application on AWS"
-    repository  = "aws-fastapi"
-    project_root = "infra"
-    branch      = "master"
-    autodeploy  = true
-    import_state_file = "states/state.tfstate"
+  name              = "AWS Fastapi"
+  description       = "Provisions a FastAPI application on AWS"
+  repository        = "aws-fastapi"
+  project_root      = "infra"
+  branch            = "master"
+  autodeploy        = true
 }
