c050c48356ca06a94bd6cc80a7a970e30b2dcde9
Aleksander Fidelus <aleksander.fidelus@hitachivantara.com>
2023-11-11 13:21:55 +0100

    Add required provider

--- main.tf
+++ main.tf
@@ -3,6 +3,10 @@ terraform {
     spacelift = {
       source  = "spacelift-io/spacelift"
       version = "1.6.0"
+    },
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.25.0"
     }
   }
 }
