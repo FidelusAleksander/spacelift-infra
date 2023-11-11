ddcc93d0e4e5fa0af7020180ae12ab12f0249ca7
Aleksander Fidelus <aleksander.fidelus@hitachivantara.com>
2023-11-11 14:11:15 +0100

    parametrize the list of attached policies

--- modules/aws_integration/variables.tf
+++ modules/aws_integration/variables.tf
@@ -7,3 +7,8 @@ variable "stack_id" {
   type        = string
   description = "The ID of the stack to which the IAM role will be attached"
 }
+
+variable "iam_policy_arns" {
+  type        = list(string)
+  description = "A list of IAM policy ARNs to attach to the IAM role"
+}
