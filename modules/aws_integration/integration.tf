4bc34368028ca031fc4911f406355ef51314167b
Aleksander Fidelus <aleksander.fidelus@hitachivantara.com>
2023-11-14 13:53:08 +0100

    space_id argument for spacelift_aws_integration

--- modules/aws_integration/integration.tf
+++ modules/aws_integration/integration.tf
@@ -4,6 +4,7 @@ resource "spacelift_aws_integration" "this" {
   # We need to set the ARN manually rather than referencing the role to avoid a circular dependency
   role_arn                       = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.role_name}"
   generate_credentials_in_worker = false
+  space_id                       = var.space_id
 }
 
 data "spacelift_aws_integration_attachment_external_id" "my_stack" {
@@ -11,10 +12,7 @@ data "spacelift_aws_integration_attachment_external_id" "my_stack" {
   stack_id       = var.stack_id
   read           = true
   write          = true
-  space_id       = var.space_id
 }
-
-
 
 # Attach the integration to any stacks or modules that need to use it
 resource "spacelift_aws_integration_attachment" "my_stack" {
