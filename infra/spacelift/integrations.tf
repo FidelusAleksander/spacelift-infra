8ffe6e75e1b88734380aef9cf179c89b16fa136f
FidelusAleksander <alek.fidelus@gmail.com>
2025-01-02 19:00:25 +0100

    Fix integrations

--- infra/spacelift/integrations.tf
+++ infra/spacelift/integrations.tf
@@ -7,29 +7,29 @@ resource "spacelift_aws_integration" "spacelift_demo" {
 }
 
 resource "spacelift_aws_integration_attachment" "spacelift_demo_core" {
-  integration_id = spacelift_aws_integration.spacelift_demo_core.id
-  stack_id       = spacelift_stack.networking.id
+  integration_id = spacelift_aws_integration.spacelift_demo.id
+  stack_id       = spacelift_stack.spacelift_demo_core.id
   read           = true
   write          = true
 }
 
 
 resource "spacelift_aws_integration_attachment" "spacelift_demo_aws_fastapi" {
-  integration_id = spacelift_aws_integration.aws_demo.id
+  integration_id = spacelift_aws_integration.spacelift_demo.id
   stack_id       = spacelift_stack.aws_fastapi.id
   read           = true
   write          = true
 }
 
 resource "spacelift_aws_integration_attachment" "spacelift_demo_storage" {
-  integration_id = spacelift_aws_integration.aws_demo.id
+  integration_id = spacelift_aws_integration.spacelift_demo.id
   stack_id       = spacelift_stack.storage.id
   read           = true
   write          = true
 }
 
 resource "spacelift_aws_integration_attachment" "spacelift_demo_networking" {
-  integration_id = spacelift_aws_integration.aws_demo.id
+  integration_id = spacelift_aws_integration.spacelift_demo.id
   stack_id       = spacelift_stack.networking.id
   read           = true
   write          = true
