4835cf96fec186f595b23e8780145ee2fb0012ad
FidelusAleksander <alek.fidelus@gmail.com>
2025-01-02 13:39:41 +0100

    Restructure files

---
+++ infra/spacelift/integrations.tf
@@ -0,0 +1,21 @@
+resource "spacelift_aws_integration" "aws_demo" {
+  name = "aws_demo_integration"
+
+  role_arn                       = "arn:aws:iam::859006480097:role/spacelift-integration-role"
+  generate_credentials_in_worker = false
+  space_id                       = spacelift_space.workloads-dev.id
+}
+
+resource "spacelift_aws_integration_attachment" "aws_demo_aws_fastapi" {
+  integration_id = spacelift_aws_integration.aws_demo.id
+  stack_id       = spacelift_stack.aws_fastapi.id
+  read           = true
+  write          = true
+}
+
+resource "spacelift_aws_integration_attachment" "aws_demo_storage" {
+  integration_id = spacelift_aws_integration.aws_demo.id
+  stack_id       = spacelift_stack.storage.id
+  read           = true
+  write          = true
+}
