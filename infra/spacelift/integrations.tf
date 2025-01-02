5892cbde745af077363659184ce786da7236ca11
FidelusAleksander <alek.fidelus@gmail.com>
2025-01-02 19:35:13 +0100

    Add integration to core shared services stack

--- infra/spacelift/integrations.tf
+++ infra/spacelift/integrations.tf
@@ -36,3 +36,19 @@ resource "spacelift_aws_integration_attachment" "spacelift_demo_networking" {
   write          = true
 }
 
+## Shared Services
+
+resource "spacelift_aws_integration" "shared_services" {
+  name = "shared_services_integration"
+
+  role_arn                       = "arn:aws:iam::619071326466:role/spacelift-role"
+  generate_credentials_in_worker = false
+  space_id                       = spacelift_space.infrastructure.id
+}
+
+resource "spacelift_aws_integration_attachment" "shared_services_core" {
+  integration_id = spacelift_aws_integration.shared_services.id
+  stack_id       = spacelift_stack.shared_services_core.id
+  read           = true
+  write          = true
+}
