4ada7a11d4272eb7bbed7d572d8f6958f0f18bf9
FidelusAleksander <alek.fidelus@gmail.com>
2025-01-02 13:33:24 +0100

    Remove aws provider

--- main.tf
+++ main.tf
@@ -4,16 +4,8 @@ terraform {
       source  = "spacelift-io/spacelift"
       version = "~> 1.5"
     }
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.24"
-    }
   }
 }
 
 provider "spacelift" {
-}
-
-provider "aws" {
-  region = "eu-west-1"
 }
