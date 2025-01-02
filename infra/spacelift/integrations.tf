a236ae8c4609136e8b7d3d59c2fc9a76bf9222a1
FidelusAleksander <alek.fidelus@gmail.com>
2025-01-02 19:07:56 +0100

    Rename integration

--- infra/spacelift/integrations.tf
+++ infra/spacelift/integrations.tf
@@ -1,5 +1,5 @@
 resource "spacelift_aws_integration" "spacelift_demo" {
-  name = "aws_demo_integration"
+  name = "spacelift_demo_integration"
 
   role_arn                       = "arn:aws:iam::859006480097:role/spacelift-role"
   generate_credentials_in_worker = false
