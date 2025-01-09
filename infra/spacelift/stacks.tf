cc1e1a661407c703f90aa1bf1d499460aa33eedb
FidelusAleksander <alek.fidelus@gmail.com>
2025-01-09 13:10:53 +0000

    Add stack dependencies

--- infra/spacelift/stacks.tf
+++ infra/spacelift/stacks.tf
@@ -90,28 +90,26 @@ module "stacks" {
 }
 
 
+# TODO: Create a module for stack dependencies
 
-# resource "spacelift_stack_dependency" "aws_fastapi_networking" {
-#   stack_id            = spacelift_stack.aws_fastapi.id
-#   depends_on_stack_id = spacelift_stack.networking.id
-# }
+resource "spacelift_stack_dependency" "aws_fastapi_networking" {
+  stack_id            = module.stacks["spacelift_demo_aws_fastapi"].stack_id
+  depends_on_stack_id = module.stacks["spacelift_demo_networking"].stack_id
+}
 
-# resource "spacelift_stack_dependency_reference" "aws_fastapi_networking_vpc_id" {
-#   stack_dependency_id = spacelift_stack_dependency.aws_fastapi_networking.id
-#   output_name         = "vpc_id"
-#   input_name          = "TF_VAR_vpc_id"
-# }
+resource "spacelift_stack_dependency_reference" "aws_fastapi_networking_vpc_id" {
+  stack_dependency_id = spacelift_stack_dependency.aws_fastapi_networking.id
+  output_name         = "vpc_id"
+  input_name          = "TF_VAR_vpc_id"
+}
 
-# resource "spacelift_stack_dependency" "aws_fastapi_storage" {
-#   stack_id            = spacelift_stack.aws_fastapi.id
-#   depends_on_stack_id = spacelift_stack.storage.id
-# }
+resource "spacelift_stack_dependency" "aws_fastapi_storage" {
+  stack_id            = module.stacks["spacelift_demo_aws_fastapi"].stack_id
+  depends_on_stack_id = module.stacks["spacelift_demo_storage"].stack_id
+}
 
-# resource "spacelift_stack_dependency_reference" "aws_fastapi_networking_s3_bucket_name" {
-#   stack_dependency_id = spacelift_stack_dependency.aws_fastapi_storage.id
-#   output_name         = "s3_bucket_name"
-#   input_name          = "TF_VAR_s3_bucket_name"
-# }
-
-
-## Shared Services
+resource "spacelift_stack_dependency_reference" "aws_fastapi_networking_s3_bucket_name" {
+  stack_dependency_id = spacelift_stack_dependency.aws_fastapi_storage.id
+  output_name         = "s3_bucket_name"
+  input_name          = "TF_VAR_s3_bucket_name"
+}
