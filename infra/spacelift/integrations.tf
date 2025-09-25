ab72009263bb043f11752dd95d64a7906bc1af7a
FidelusAleksander <alek.fidelus@gmail.com>
2025-09-25 10:08:51 +0200

    Change integration, remove moved blocks

--- infra/spacelift/integrations.tf
+++ infra/spacelift/integrations.tf
@@ -2,7 +2,7 @@
 resource "spacelift_aws_integration" "spacelift_demo" {
   name = "spacelift_demo_integration"
 
-  role_arn                       = "arn:aws:iam::859006480097:role/spacelift-role"
+  role_arn                       = "arn:aws:iam::859006480097:role/spacelift-integration-role"
   generate_credentials_in_worker = false
   space_id                       = module.workload_spaces.spaces["dev"].id
 }
@@ -12,7 +12,7 @@ resource "spacelift_aws_integration" "spacelift_demo" {
 resource "spacelift_aws_integration" "shared_services" {
   name = "shared_services_integration"
 
-  role_arn                       = "arn:aws:iam::619071326466:role/spacelift-role"
+  role_arn                       = "arn:aws:iam::619071326466:role/spacelift-integration-role"
   generate_credentials_in_worker = false
   space_id                       = module.root_spaces.spaces["infrastructure"].id
 }
